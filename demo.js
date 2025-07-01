07b25170e9b512da85923c5a396308685512d924
randywanyoike <randywanyoike@gmail.com>
2025-07-01 10:31:22 +0300

    complete lab

--- demo.js
+++ demo.js
@@ -14,7 +14,7 @@ const colorStates = {
 // we want to make clickable. Without JavaScript, clicking on these heart shapes
 // does nothing. Uncomment the code and refresh the demo page. 
 
-// const articleHearts = document.querySelectorAll(".like-glyph");
+const articleHearts = document.querySelectorAll(".like-glyph");
 
 function likeCallback(e) {
   const heart = e.target;
@@ -38,9 +38,9 @@ function likeCallback(e) {
 // work, we need to add a click event listener to the elements we identified in
 // STEP 1. That's Pillar 2, event handling. Uncomment this code:
 
-// for (const glyph of articleHearts) {
-//   glyph.addEventListener("click", likeCallback);
-// }
+for (const glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
 // STEP 4: 
 
@@ -56,7 +56,12 @@ function likeCallback(e) {
 function mimicServerCall() {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
-      resolve("Pretend remote server notified of action!");
+      let isRandomFailure = Math.random() < .2
+      if (isRandomFailure) {
+        reject("Random server error. Try again.");
+      } else {
+        resolve("Pretend remote server notified of action!");
+      }
     }, 300);
   });
 }
